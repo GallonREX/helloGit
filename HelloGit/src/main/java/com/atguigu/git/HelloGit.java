5afec33506c960655080545c5909f368e701e2fe
zoujialun <810925015@qq.com>
2023-01-14 16:21:38 +0800

    zoujialun init

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author: zoujialun
+ * @Date: 2023/01/14/16:01
+ * @version:
+ * @Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
